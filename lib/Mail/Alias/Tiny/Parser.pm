cd757abd6b4642f096db90f7720c783cd87b4b0e
Erin Schoenhals <erin@cpanel.net>
2011-02-14 19:15:33 -0600

    Fixed snags in properly parsing ~/.forward and aliases declarations in separate manners; a bit more code, but cleaner this way

--- lib/Mail/Alias/Tiny/Parser.pm
+++ lib/Mail/Alias/Tiny/Parser.pm
@@ -7,52 +7,78 @@ use Mail::Alias::Tiny::Token;
 
 use Carp;
 
-sub parse {
-    my ($class, $statement, $mode) = @_;
-    my @tokens = Mail::Alias::Tiny::Token->tokenize($statement);
+sub _parse_forward_statement {
+    my ($tokens) = @_;
+    my @destinations;
 
     my $last_token = Mail::Alias::Tiny::Token->new('T_BEGIN');
-    my ($name, @destinations);
 
-    foreach my $token (@tokens) {
-        next if $token->isa(qw/T_BEGIN T_COMMENT/);
+    foreach my $token (@{$tokens}) {
+        next if $token->isa(qw/T_BEGIN T_COMMENT T_WHITESPACE/);
 
-        if ($last_token->isa('T_BEGIN')) {
-            confess("Expected address as name of alias, found $token->{'type'}") unless $token->isa('T_ADDRESS');
-        }
-        
-        if ($token->isa('T_COMMA')) {
-            confess('Unexpected comma') unless $last_token->is_value;
-        } elsif ($token->isa('T_COLON')) {
-            confess('Unexpected colon in .forward statement') if $mode eq 'forward';
-            confess('Misplaced colon') unless $token == $tokens[2];
-            confess('Unexpected colon') unless $last_token->isa('T_ADDRESS');
-
-            $name = $last_token->{'value'};
-        } elsif ($token->isa('T_WHITESPACE')) {
-            confess('Unexpected whitespace') unless $last_token->is_punct;
-        } elsif ($token->is_value) {
-            confess("Unexpected literal value '$token->{'value'}'") unless $last_token->is_punct;
+        if ($token->is_value) {
+            confess('Unexpected value') unless $last_token->is_punct;
 
             push @destinations, $token;
+        } elsif ($token->isa('T_COMMA')) {
+            confess('Unexpected comma') unless $last_token->is_value;
         } elsif ($token->isa('T_END')) {
-            confess("Unexpected end of alias statement, found $last_token->{'type'}") unless $last_token->is_value;
+            confess('Unexpected end of statement') unless $last_token->is_value;
 
             last;
+        } else {
+            confess("Unexpected $token->{'type'}");
         }
 
         $last_token = $token;
     }
 
-    confess('Declaration has no destinations') unless @destinations;
+    confess('Statement contains no destinations') unless @destinations;
 
-    if ($mode eq 'aliases') {
-        confess('No alias name specified') unless defined $name;
-    } elsif ($mode eq 'forward') {
-        return \@destinations;
+    return \@destinations;
+}
+
+sub _parse_aliases_statement {
+    my ($tokens) = @_;
+    my ($name, @destinations);
+
+    my $last_token = Mail::Alias::Tiny::Token->new('T_BEGIN');
+
+    foreach my $token (@{$tokens}) {
+        next if $token->isa(qw/T_BEGIN T_COMMENT T_WHITESPACE/);
+
+        if ($last_token->isa('T_BEGIN')) {
+            confess("Expected address as name of alias, found $token->{'type'}") unless $token->isa('T_ADDRESS');
+
+            $name = $token->{'value'};
+        } elsif ($token->isa('T_COLON')) {
+            confess('Unexpected colon') unless $last_token->isa('T_ADDRESS');
+        } elsif ($token->isa('T_COMMA')) {
+            confess('Unexpected comma') unless $last_token->is_value;
+        } elsif ($token->isa('T_END')) {
+            confess('Unexpected end of aliases statement') unless $last_token->is_value;
+
+            last;
+        } elsif ($token->is_value) {
+            push @destinations, $token;
+        }
+
+        $last_token = $token;
     }
 
+    confess('Aliases statement has no destinations') unless @destinations;
+
     return ($name, \@destinations);
+}
+
+sub parse {
+    my ($class, $statement, $mode) = @_;
+    my @tokens = Mail::Alias::Tiny::Token->tokenize($statement);
+
+    return _parse_forward_statement(\@tokens) if $mode eq 'forward';
+    return _parse_aliases_statement(\@tokens) if $mode eq 'aliases';
+
+    confess("Invalid parsing mode $mode specified");
 }
 
 1;
